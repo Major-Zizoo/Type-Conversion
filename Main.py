6099773c2b9f22d95a47d5ae518db5531a903a6f
Major-Zizoo <norelgndy@gmail.com>
2024-10-27 16:40:23 +0300

    Type Conversion

---
+++ Main.py
@@ -0,0 +1,15 @@
+Name="Malak and,Mazen are both hunmans"
+print(type(Name))
+
+Age=10
+Age=str(Age)
+print(type(Age))
+
+Height=12.4
+Height=int(Height)
+print(Height)
+print(type(Height))
+
+Student=True
+student=str(Student)
+print(type(Student))
